96ea8148552efc32e00bfbaa05bee0fd31e35490
Lucas Medina Bosso <lmbosso2008@gmail.com>
2025-02-23 22:11:25 -0300

    formulario_3

--- dist/pages/script/formulario.js
+++ dist/pages/script/formulario.js
@@ -1,10 +1,24 @@
 const outro = document.getElementById('outro');
 const outroText = document.getElementById('outro-contratante');
+const nome = document.getElementById('nome');
+const dataCasamento = document.getElementById('data-casamento');
+const equipe = document.getElementById('equipe');
+const btnEnviar = document.getElementById('btn-enviar');
+
 outro.addEventListener('click', (evento) => {
     evento.preventDefault();
   if (outro.checked) {
     outroText.style.display = 'block';
-  } else if (!outro.checked) {
-    outroText.style.display = 'none';
-  }
+  } 
+});
+
+btnEnviar.addEventListener('click', (evento) => {
+    evento.preventDefault();
+
+    const fone = '5518997541998';
+    const mensagem = `Olá, meu nome é ${nome.value} e gostaria de contratar a equipe para o dia ${dataCasamento.value}, com a equipe de ${equipe.value}.`;
+    const mensagemFinal = encodeURIComponent(mensagem); // Codifica a mensagem corretamente
+    const WhatsApp = `https://api.whatsapp.com/send?phone=${fone}&text=${mensagemFinal}`; // Adiciona o & corretamente
+
+    window.open(WhatsApp);
 });
