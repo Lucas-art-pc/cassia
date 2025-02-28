28ad39331e026f99fffccd17837cf19beb762414
Lucas Medina Bosso <lmbosso2008@gmail.com>
2025-02-27 21:55:13 -0300

    término formulario e inicio da aba equipes

--- dist/pages/script/formulario.js
+++ dist/pages/script/formulario.js
@@ -1,24 +1,47 @@
-const outro = document.getElementById('outro');
-const outroText = document.getElementById('outro-contratante');
+const contratante = document.getElementById('contratante');
+const outroDiv = document.getElementById('outro-contratante-div');
+const nomeOutro = document.getElementById('outro-contratante');
 const nome = document.getElementById('nome');
 const dataCasamento = document.getElementById('data-casamento');
 const equipe = document.getElementById('equipe');
 const btnEnviar = document.getElementById('btn-enviar');
 
-outro.addEventListener('click', (evento) => {
-    evento.preventDefault();
-  if (outro.checked) {
-    outroText.style.display = 'block';
-  } 
-});
+function mostrarOutro() {
+    outroDiv.style.display = contratante.value === 'outro' ? 'block' : 'none';
+}
+
+contratante.addEventListener('change', mostrarOutro);
 
 btnEnviar.addEventListener('click', (evento) => {
     evento.preventDefault();
 
+    // Verifica se os campos essenciais estão preenchidos
+    if (!nome.value || !dataCasamento.value || !equipe.value || !contratante.value) {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+        return;
+    }
+
     const fone = '5518997541998';
-    const mensagem = `Olá, meu nome é ${nome.value} e gostaria de contratar a equipe para o dia ${dataCasamento.value}, com a equipe de ${equipe.value}.`;
-    const mensagemFinal = encodeURIComponent(mensagem); // Codifica a mensagem corretamente
-    const WhatsApp = `https://api.whatsapp.com/send?phone=${fone}&text=${mensagemFinal}`; // Adiciona o & corretamente
+    let contratanteTexto = contratante.value;
+
+    // Se for "outro", adiciona o nome do contratante informado
+    contratante === 'outro' ? contratanteTexto = nomeOutro.value : contratanteTexto = nomeOutro.value;
+
+    const dataHoje = new Date();
+    const dataSelecionada = new Date(dataCasamento.value);
+    
+    // Ajuste para considerar apenas o dia, mês e ano (removendo horas, minutos e segundos)
+    dataHoje.setHours(0, 0, 0, 0);
+    dataSelecionada.setHours(0, 0, 0, 0);
+    
+    if (dataSelecionada <= dataHoje) {
+        alert("Por favor, informe uma data futura.");
+        return;
+    }
+
+    const mensagem = `Olá, meu nome é ${nome.value}, sou ${contratanteTexto} e gostaria de contratar a equipe para o dia ${dataCasamento.value}, com a equipe de ${equipe.value}.`;
+    const mensagemFinal = encodeURIComponent(mensagem);
+    const WhatsApp = `https://api.whatsapp.com/send?phone=${fone}&text=${mensagemFinal}`;
 
     window.open(WhatsApp);
 });
